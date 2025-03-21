440e7101b45a7694c68549b8483c69b43624a506
Andreas Siggelin <andsig@wcom.se>
2025-03-21 13:05:11 +0100

    initiated testscenarios

---
+++ src/AZDOI.Tests/Unit/Markdown/TestMarkdownServiceTestsWithoutIOC.cs
@@ -0,0 +1,40 @@
+﻿using Cake.Core;
+using Cake.Core.IO;
+using Microsoft.Extensions.Time.Testing;
+using NSubstitute;
+
+namespace AZDOI.Tests.Unit.Markdown;
+
+public class TestMarkdownServiceTestsWithoutIOC
+{
+    [Fact]
+    public async Task WriteIndex_ShouldWriteExpectedMarkdownContent()
+    {
+        // Given
+        //var (fileSystem, service) = ServiceProviderFixture.GetRequiredService<FakeFileSystem, TestMarkdownService>();
+        ICakeEnvironment environment = FakeEnvironment.CreateUnixEnvironment();
+        IFileSystem fileSystem = new FakeFileSystem(environment);
+        ICakeContext context = Substitute.For<ICakeContext>();
+        context.FileSystem.Returns(fileSystem);
+        var timeProvider = new FakeTimeProvider();
+        var service = new TestMarkdownService(
+            context,
+            timeProvider
+            );
+
+        string markdownContent = "# MyOrg DevOps Organization";
+
+        // When
+        var result = await service.TestWriteIndex(markdownContent);
+
+        // Then
+        await Verify(
+            new
+            {
+                Content = result,
+                Equal = markdownContent == result,
+                FileExists = fileSystem.GetFile("/index.md").Exists
+            }
+        );
+    }
+}
