a32d5a354ffad0151019cee206fe0855d53c6482
Andreas Siggelin <andsig@wcom.se>
2025-03-21 13:05:11 +0100

    initiated testscenarios

---
+++ src/AZDOI.Tests/Unit/Markdown/TestMarkdownServiceTestsWithoutAnything.cs
@@ -0,0 +1,42 @@
+﻿using Cake.Core;
+using Cake.Core.IO;
+using Microsoft.Extensions.Time.Testing;
+using NSubstitute;
+
+namespace AZDOI.Tests.Unit.Markdown;
+
+public class TestMarkdownServiceTestsWithoutAnything
+{
+    [Fact]
+    public async Task WriteIndex_ShouldWriteExpectedMarkdownContent()
+    {
+        // Given
+        //var (fileSystem, service) = ServiceProviderFixture.GetRequiredService<FakeFileSystem, TestMarkdownService>();
+        ICakeEnvironment environment = FakeEnvironment.CreateUnixEnvironment();
+        IFileSystem fileSystem = new FakeFileSystem(environment);
+        ICakeContext context = Substitute.For<ICakeContext>();
+        context.FileSystem.Returns(fileSystem);
+        var timeProvider = new FakeTimeProvider();
+        var service = new TestMarkdownService(
+            context,
+            timeProvider
+            );
+        const string markdownContent = "# MyOrg DevOps Organization";
+        const string expect =
+            $$"""
+                ---
+                modifiedby: AZDOI
+                modified: 2000-01-01 00:00
+                ---
+                {{markdownContent}}
+                """;
+
+        // When
+        var result = await service.TestWriteIndex(markdownContent);
+        var file = fileSystem.GetFile("/index.md");
+
+        // Then
+        Assert.Equal(expect, result);
+        Assert.True(file.Exists, "File doesn't exist.");
+    }
+}
---
+++ src/AZDOI.Tests/Unit/Markdown/TestMarkdownServiceTestsWithoutSnapshot.cs
@@ -0,0 +1,28 @@
+﻿namespace AZDOI.Tests.Unit.Markdown;
+
+public class TestMarkdownServiceTestsWithoutSnapshot
+{
+    [Fact]
+    public async Task WriteIndex_ShouldWriteExpectedMarkdownContent()
+    {
+        // Given
+        var (fileSystem, service) = ServiceProviderFixture.GetRequiredService<FakeFileSystem, TestMarkdownService>();
+        const string markdownContent = "# MyOrg DevOps Organization";
+        const string expect =
+            $$"""
+                ---
+                modifiedby: AZDOI
+                modified: 2000-01-01 00:00
+                ---
+                {{markdownContent}}
+                """;
+
+        // When
+        var result = await service.TestWriteIndex(markdownContent);
+        var file = fileSystem.GetFile("/index.md");
+
+        // Then
+        Assert.Equal(expect, result);
+        Assert.True(file.Exists, "File doesn't exist.");
+    }
+}
